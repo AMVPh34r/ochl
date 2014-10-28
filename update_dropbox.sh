d32d2550a973f4c971969f26daf4d9f1c8247b2c
Alex Schaeffer <AMVPh34r@gmail.com>
2014-10-28 17:31:13 -0400

    Ignore custom shell scripts

--- update_dropbox.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-DIR="/home/alex/Dropbox/School/UNCC/Y3/S1/ENGL-2116/Unit 3/site"
-
-echo "Copying contents of \"$(pwd)\" to \"${DIR}\"..."
-rsync -rulv --delete --exclude="$(basename $0)" --exclude=".git" . "${DIR}"
