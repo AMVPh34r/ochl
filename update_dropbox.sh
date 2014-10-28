dcad5b504219fa63086415b72f8a2e42fe9d748f
Alex Schaeffer <AMVPh34r@gmail.com>
2014-10-28 17:28:51 -0400

    Updated script to ignore git folder

--- update_dropbox.sh
+++ update_dropbox.sh
@@ -3,4 +3,4 @@
 DIR="/home/alex/Dropbox/School/UNCC/Y3/S1/ENGL-2116/Unit 3/site"
 
 echo "Copying contents of \"$(pwd)\" to \"${DIR}\"..."
-rsync -rulv --delete --exclude="$(basename $0)" . "${DIR}"
+rsync -rulv --delete --exclude="$(basename $0)" --exclude=".git" . "${DIR}"
