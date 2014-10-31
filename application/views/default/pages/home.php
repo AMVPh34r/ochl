a8c5d9a1ab21dc889f909b96e7f6c34e10bbb13c
Alex Schaeffer <AMVPh34r@gmail.com>
2014-10-31 10:39:44 -0400

    Fixed carousel - moved js in to its own file

--- application/views/default/pages/home.php
+++ application/views/default/pages/home.php
@@ -121,9 +121,4 @@
 </div>
 <!-- /.container -->
 
-<!-- Script to Activate the Carousel -->
-<script>
-$('.carousel').carousel({
-    interval: 5000 //changes the speed
-})
-</script>
+<?php $GLOBALS['js_load'][] = 'home.js'; ?>
