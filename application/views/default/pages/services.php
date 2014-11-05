022444a2133b52307c9cbde5dee4e13865c54aa0
Alex Schaeffer <AMVPh34r@gmail.com>
2014-11-05 10:04:41 -0500

    Added header image

--- application/views/default/pages/services.php
+++ application/views/default/pages/services.php
@@ -18,7 +18,8 @@
     <!-- Image Header -->
     <div class="row">
         <div class="col-lg-12">
-            <img class="img-responsive" src="http://placehold.it/1200x300" alt="">
+            <!-- <img class="img-responsive" src="http://placehold.it/1200x300" alt=""> -->
+            <?php echo img(array('src'=>'files/img/static/hitchhikers_lg.png', 'class'=>'img-responsive')); ?>
         </div>
     </div>
     <!-- /.row -->
@@ -31,35 +32,35 @@
         <div class="col-lg-12">
 
             <ul id="tab-nav" class="nav nav-tabs nav-justified">
-                <li class="active"><a href="#service-one" data-toggle="tab"><i class="fa fa-tree"></i> Weekly Meetings</a>
+                <li class="active"><a href="#service-one" data-toggle="tab"><i class="fa fa-graduation-cap"></i> Learn Stuff</a>
                 </li>
-                <li class=""><a href="#service-two" data-toggle="tab"><i class="fa fa-car"></i> Special Events</a>
+                <li class=""><a href="#service-two" data-toggle="tab"><i class="fa fa-group"></i> Weekly Meetings</a>
                 </li>
-                <li class=""><a href="#service-three" data-toggle="tab"><i class="fa fa-support"></i> Build Sessions</a>
+                <li class=""><a href="#service-three" data-toggle="tab"><i class="fa fa-slideshare"></i> Special Events</a>
                 </li>
-                <li class=""><a href="#service-four" data-toggle="tab"><i class="fa fa-database"></i> Learn Stuff</a>
+                <li class=""><a href="#service-four" data-toggle="tab"><i class="fa fa-desktop"></i> Build Sessions</a>
                 </li>
             </ul>
 
             <div id="tab-content" class="tab-content">
                 <div class="tab-pane fade active in" id="service-one">
-                    <h4>Weekly Meetings</h4>
-                    <p>Our weekly meetings are well suited for both students who are new to hardware, and veterans. We go over the basics of what hardware does, and talk about new parts that are on the market.</p>
+                <h4>Learn Stuff</h4>
+                    <p>It's out goal, no matter how much you know about computers, if you come to an OCHL meeting, you'll walk away from it with something you didn't know before. Because we have so many leaders in OCHL, we can talk with new members about the basics, and others about new advances or products.</p>
                     <p></p>
                 </div>
                 <div class="tab-pane fade" id="service-two">
+                <h4>Weekly Meetings</h4>
+                    <p>Our weekly meetings are well suited for both students who are new to hardware, and veterans. We go over the basics of what hardware does, and talk about new parts that are on the market.</p>
+                    <p></p>
+                </div>
+                <div class="tab-pane fade" id="service-three">
                     <h4>Special Events</h4>
                     <p>We will have a guest speaker when ever we can. We have scheduled for representatives from Intel to come and speak about their new products.</p>
                     <p></p>
                 </div>
-                <div class="tab-pane fade" id="service-three">
+                <div class="tab-pane fade" id="service-four">
                     <h4>Build Sessions</h4>
                     <p>During these sessions we will either be building a computer in person, or will be building one online. The goal of in person builds is to help our new members build their machines, and the purpose of the online builds are to see who can build the best PC in a given budget.</p>
-                    <p></p>
-                </div>
-                <div class="tab-pane fade" id="service-four">
-                    <h4>Learn Stuff</h4>
-                    <p>It's out goal, no matter how much you know about computers, if you come to an OCHL meeting, you'll walk away from it with something you didn't know before. Because we have so many leaders in OCHL, we can talk with new members about the basics, and others about new advances or products.</p>
                     <p></p>
                 </div>
             </div>
