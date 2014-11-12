2a3787cc857221ac1e6aaf4ddf8f2bbc6ce74df5
Alex Schaeffer <AMVPh34r@gmail.com>
2014-11-12 10:26:21 -0500

    Added placeholders for remaining guide pages

---
+++ application/views/default/pages/guides/choosing-computer-components/6.php
@@ -0,0 +1,11 @@
+<?php
+$active = 6;
+
+include('data.php');
+include('header.php');
+?>
+    
+<h2>Page 6 Heading</h2>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta, et temporibus, facere perferendis veniam beatae non debitis, numquam blanditiis necessitatibus vel mollitia dolorum laudantium, voluptate dolores iure maxime ducimus fugit.</p>
+
+<?php include('footer.php'); ?>
---
+++ application/views/default/pages/guides/choosing-computer-components/7.php
@@ -0,0 +1,11 @@
+<?php
+$active = 7;
+
+include('data.php');
+include('header.php');
+?>
+    
+<h2>Page 7 Heading</h2>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta, et temporibus, facere perferendis veniam beatae non debitis, numquam blanditiis necessitatibus vel mollitia dolorum laudantium, voluptate dolores iure maxime ducimus fugit.</p>
+
+<?php include('footer.php'); ?>
