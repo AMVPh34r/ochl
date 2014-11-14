a0fd6d3277a9d83a008245e1d48085144dceda43
Alex Schaeffer <AMVPh34r@gmail.com>
2014-11-14 11:31:05 -0500

    Added animated drop shadow to navbar

--- application/views/default/navbar.php
+++ application/views/default/navbar.php
@@ -12,7 +12,7 @@ $navbar_links = array(
 ?>
 
 <!-- Navigation -->
-<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+<nav id="main-nav" class="navbar navbar-inverse navbar-fixed-top" role="navigation">
     <div class="container">
         <div class="navbar-header">
             <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse-1">
---
+++ styles/default/js/script.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+	$('#main-nav').affix({
+		offset: {top:0}
+	});
+});
