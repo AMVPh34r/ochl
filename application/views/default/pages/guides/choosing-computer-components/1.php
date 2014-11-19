878ab6c79f2c87804719256eba4cf5f8ec0aaca0
Alex Schaeffer <AMVPh34r@gmail.com>
2014-11-19 10:37:39 -0500

    Finished intro page

--- application/views/default/pages/guides/choosing-computer-components/1.php
+++ application/views/default/pages/guides/choosing-computer-components/1.php
@@ -17,17 +17,22 @@ $next_url = site_url('guides/'.$guide_uri.'/2');
 
 <h4>Free of bloatware.</h4>
 <p>
-	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse consectetur consectetur urna sit amet pulvinar. Nullam venenatis luctus turpis, in dictum lacus commodo vitae. Fusce egestas vel dolor nec dapibus. Maecenas blandit, neque at eleifend bibendum, ante libero fringilla sem, fermentum dignissim magna sapien ac ligula. Suspendisse condimentum vel urna a elementum. Quisque ac pharetra nibh. Fusce at pulvinar ante. Sed nec sapien in ipsum tempus ultrices. Sed nisl arcu, bibendum vel sodales laoreet, luctus id magna. Nullam vel volutpat mi.
+	When you buy a computer from the store, you pay for the convenience. Most tech stores install a host of widgets and applications on all their computers before selling them. These applications serve purposes ranging from virus scanners to hardware monitors, and while that may sound useful, more often than not the programs only end up slowing the computer down (hence the name "bloatware"), and there are always better alternative programs that serve the same purposes. On top of that, some applications actively monitor computer usage and send that information back to the store, so if you are concerned about privacy you likely won't want these programs on your computer. By building your own computer, you get to install the operating system yourself, providing you the slimmest and fastest out-of-the-box experience.
 </p><br />
 
 <h4>Better price/performance.</h4>
 <p>
-	Mauris et mollis dui. Integer congue nec lacus vel placerat. Vestibulum sit amet nisi aliquam, facilisis tortor a, aliquet dolor. Aenean consequat sit amet leo et pretium. Suspendisse dui sem, varius non sapien sed, tincidunt tempus justo. Sed lacinia, massa vehicula egestas vestibulum, orci nibh tristique metus, in congue libero metus vel urna. Curabitur eget nibh sed ex porta accumsan et quis ex. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+	Perhaps the most convincing argument for building your own PC is price. When you buy from a retailer, they have already bought their computers from the computer manufacturer, who has in turn bought the individual parts from several other manufacturers. The end result is a significant markup in price. By buying individal components yourself, you can cut out the middleman and get a much more powerful computer for the same price, or settle for less and save yourself some money.
 </p><br />
 
 <h4>You can make it yours.</h4>
 <p>
-	Vivamus auctor convallis lectus, ut volutpat massa. Nunc id faucibus lorem, ut dictum purus. Morbi ac feugiat dui. Cras venenatis est eu justo mollis, et luctus augue semper. Integer egestas magna vel molestie condimentum. Mauris varius nisi mauris, et molestie nisl vestibulum eu. Proin blandit maximus erat sed suscipit. Phasellus ut molestie nulla. Proin a massa eget sapien consectetur suscipit.
+	The most you're likely to get in terms of customizability from a store is a few different case sizes, and maybe some color choices. When building yourself, you get to choose each individual part that goes into the computer, giving you the ability to create exactly what you need or want. Need more hard drive space? Not a problem. Want a window in the case so you can see inside? You can do that. Building your own PC gives you the absolute maximum in customizability.
+</p>
+
+<hr />
+<p>
+	Ready to get started? Excellent, go ahead and click the button below to continue on to the first step: choosing your processor.
 </p>
 
 <?php include('footer.php'); ?>
