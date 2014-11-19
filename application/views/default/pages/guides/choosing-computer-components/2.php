06fd6065ce83383ffed06883c580f13b74e6fab3
Alex Schaeffer <AMVPh34r@gmail.com>
2014-11-19 16:03:51 -0500

    "Finished" CPU page

--- application/views/default/pages/guides/choosing-computer-components/2.php
+++ application/views/default/pages/guides/choosing-computer-components/2.php
@@ -7,38 +7,36 @@ include('header.php');
 
 <h2>What is the Processor?</h2>
 <p>
-	Here we will explain the purpose and importance of the CPU, what it does and how the computer uses it. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis tristique eleifend ligula. Etiam lobortis aliquet rhoncus. Cras condimentum rutrum tortor sit amet consectetur. Quisque rutrum tellus libero, eget ultricies justo elementum eleifend. Integer maximus sollicitudin lobortis. Praesent tincidunt, nisi elementum ullamcorper lacinia, neque urna vehicula ligula, sit amet auctor odio mauris quis nisi. Aenean efficitur ac elit quis tempus. Praesent dignissim leo dolor. Vivamus tincidunt tortor sed nibh ornare, non cursus mi auctor. Aliquam rhoncus, enim eu tincidunt faucibus, arcu tortor vehicula orci, nec volutpat enim felis sodales nulla. Suspendisse vestibulum ultricies nunc eu sagittis.
+	The Central Processing Unit, often called the Processor or CPU, is one of the most important components. It's what makes a computer compute. Every time a computer needs to perform an operation, as basic as moving the mouse to complex calculations in video games, the processor does the work. There are many different existing types of processors, built for different tasks. Some are meant for general use and are less powerful, others made for video editing or gaming, still others made to be used in online servers. So how do you know which is best for you?
 </p> 
 
 <h2>Picking a Brand</h2>
 <p>
-	State the two main CPU manufacturers, Intel and AMD. Explain the differences between the two and make note of compatibility issues. Also touch on how they work with other hardware (Intel/Nvidia vs AMD/Radeon). Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla condimentum quis eros non placerat. Proin sollicitudin, sapien in porttitor hendrerit, nisi ante elementum eros, vel bibendum neque odio vel felis. Nam at sagittis risus, quis rhoncus justo. Duis tellus nibh, aliquet ut ligula sit amet, ullamcorper commodo nisl. Cras feugiat sapien in tellus rhoncus, in scelerisque elit pretium. Nunc volutpat lectus ut lobortis euismod. Cras et risus nec odio bibendum ornare. Vestibulum vitae urna elementum, congue velit gravida, vestibulum sapien. Duis non tortor at tortor tincidunt posuere a efficitur felis.
+	There are two main manufacturers of CPUs: Intel and Advanced Micro Devices (AMD). Which is best for you depends on what kind of PC you're looking to build, and what your priorities are. Even within the two brands, there is a huge variety of CPU models, but a simple general rule is that Intel processors are more powerful at a higher price, and AMD processors, while overall less powerful, are more fairly priced. If money is less of a hinderance and you want the most powerful computer, Intel is the way to go. If you care more about saving money or don't need a blazing fast machine, AMD will suit you just fine.
 </p>
-<ul>
-	<li>AMD is generally for low budget builds.</li>
-	<li>Intel’s are more powerful and more expensive.</li>
-	<li>Intel CPUs are not compatible with AMD motherboards, and vice versa.</li>
-</ul>
+<p>
+	One note of importance is that Intel and AMD processors differ in their physical builds. Which brand you choose (and to an extent, which model) will affect what other hardware you can choose from due to compatibility limitations. This will be covered in greater detail in the graphics cards section of this guide.
+</p>
 
 <h2>How Many Cores?</h2>
 <p>
-	Describe the basic idea of a CPU "core" and how number of cores affects CPU power. Possibly touch on threading. Sed rutrum felis felis, ut malesuada lectus placerat vitae. Phasellus sit amet elit lectus. Curabitur hendrerit neque quis vehicula cursus. In cursus nulla non tortor mollis auctor. Proin at nibh ligula. Proin dictum tellus eu felis pretium lacinia. Cras laoreet, dolor in volutpat pretium, arcu sapien condimentum ante, ac rhoncus metus felis id elit. Nam eu facilisis eros. Sed sed enim id turpis ornare semper non id nulla. Mauris tempus vestibulum mauris ac tristique. Duis mollis lorem at leo pellentesque efficitur. Nulla suscipit orci ac elit auctor hendrerit. Curabitur ut sapien cursus, fringilla libero sit amet, vestibulum leo. Maecenas vitae ante tristique, tincidunt quam convallis, elementum sem.
+	All modern processors contain multiple microprocessors, allowing for computing work to be broken down and shared among them, dramatically increasing speed and efficiency. These are called multi-core processors, and have become the standard for all computers as well as tablets and smartphones.
+</p>
+<p>
+	When choosing the processor for your computer, you should decide how many cores you need. Objectively deciding the number of cores based on your expected usage is complicated, so here's a very simplified guide to help you make that decision:
 </p>
 <ul>
-	<li>If you just plan on surfing the internet, 2 cores are adequate, but you will be limited in what you can do.</li>
-	<li>If you want to do editing, you will need 4 or more.</li>
-	<li>General rule is more cores are better.</li>
+	<li>If you just plan on surfing the internet and doing simple text-based work (e.g. word processing, spreadsheets, etc.), 2 cores are adequate, but you will be limited in what you can do. For comparison, some laptops and most smartphones use dual-core processors.</li>
+	<li>If you want to do more intensive work such as photo or video editing, or gaming, you will need at least a quad-core (4-core) processor.</li>
+	<li>While processors with up to 12 cores exist, you will almost certainly never need such a powerful processor in day-to-day computing. This is only an option to consider if you simply want the most powerful machine you can possibly build.</li>
 </ul>
 
 <h2>Choosing the Speed</h2>
-<p>
-	Explain how CPU speed is measured and how core number factors into this speed. Ut porttitor sapien nec pellentesque lacinia. Phasellus facilisis rutrum dolor et facilisis. Maecenas orci mauris, dictum in hendrerit nec, rutrum a sem. Maecenas sed eleifend magna. Integer posuere est vel metus tincidunt fringilla at vitae ipsum. Mauris nec ornare ante. Mauris eget molestie ex, in auctor mauris. Sed nisl nisl, lacinia in tellus vel, sollicitudin consequat nulla. Pellentesque hendrerit tempus purus, non venenatis est laoreet sed. Phasellus et magna non magna bibendum aliquam in sit amet quam. Etiam et vehicula odio.
-</p>
 <ul>
 	<li>Gigahertz, or Ghz is the speed of the processor.</li>
-	<li>If you plan on editing, you will want more then 3.0</li>
-	<li>If you plan in gaming, you will want more then 2.5</li>
-	<li>If you just plan of surfing the Internet, 2.0 is fine.</li>
+	<li>If you plan on editing, you will want a processor with more then 3.0GHz.</li>
+	<li>If you plan on gaming, you will want more then 2.5.</li>
+	<li>If you just plan on surfing the Internet, 2.0 is fine.</li>
 </ul>
 
 <?php include('footer.php'); ?>
