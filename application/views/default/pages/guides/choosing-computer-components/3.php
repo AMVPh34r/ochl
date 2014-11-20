8adc899a4677880bf0938e9121513d97d22a499f
Alex Schaeffer <AMVPh34r@gmail.com>
2014-11-20 12:40:01 -0500

    "Finished" motherboard and RAM pages

--- application/views/default/pages/guides/choosing-computer-components/3.php
+++ application/views/default/pages/guides/choosing-computer-components/3.php
@@ -7,7 +7,7 @@ include('header.php');
     
 <h2>What is the Motherboard?</h2>
 <p>
-	Here we will explain the purpose of the motherboard. Discuss features of some motherboards and possibly touch on the BIOS. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta, et temporibus, facere perferendis veniam beatae non debitis, numquam blanditiis necessitatibus vel mollitia dolorum laudantium, voluptate dolores iure maxime ducimus fugit.
+	The motherboard is the "main hub" of the rest of the computer. Its job is to take input from and send output to all other components. And while it doesn't do much work itself past routing electricity, it's one of the most important components in any computer. Thankfully, there isn't much variation in motherboard types, so picking out the one that's right for you is simple.
 </p>
 
 <h2>Picking a Size</h2>
@@ -21,15 +21,12 @@ include('header.php');
 	</div>
 </div>
 <p>
-	Discuss the different motherboard sizes (ATX, Extended/ini/micro ATX) and the advantages and disadvantages of each. Point out compatibility with different case sizes. Curabitur scelerisque ipsum ut tellus blandit consequat. Morbi blandit sollicitudin elit eget molestie. Vivamus tincidunt augue at odio blandit, a facilisis massa mattis. Aenean consectetur justo ut sem pulvinar cursus.
-</p>
-<p>
-	Motherboards come in different sizes; this and the case determine the size of your computer. A Mini ATX board fits in a Mini ATX case, but can be used in bigger cases. An Extended ATX board will fit in an Extended ATX case, but noting smaller.
+	The biggest difference between different motherboards is their size. There are six different standard sizes used in modern computers: FlexATX, MicroATX, Mini ATX, Standard ATX, Extended ATX and Workstation ATX. Picking the size that's right for you is as simple as deciding how big you want your computer to be. Flex and Mico ATX motherboards are made for smaller cases, which is useful if you want to be able to move your computer around with ease. Standard and Extended ATX boards provide more room for other components, extra slots for graphics cards, and are intended for larger cases. Keep in mind if you want to get a powerful graphics card (or multiple graphics cards) or use water cooling in your computer, you will likely need a larger motherboard.
 </p>
 
 <h2>Extra Features</h2>
 <p>
-	Talk about specific features to some models. Things such as built in WiFi, Bluetooth, video outputs, etc. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut bibendum aliquam turpis, quis eleifend arcu bibendum ac. Vivamus ultricies non dui quis malesuada. Ut aliquam mauris eget elit lobortis volutpat. Fusce lectus lorem, pulvinar sit amet lorem in, rhoncus condimentum lorem. Curabitur tincidunt nibh ut nisl tempus, nec sagittis ante rhoncus.
+	Some motherboards come with a few extra features not standard in all computers. These can range from built-in WiFi and Bluetooth, to extra video outputs for multiple monitors. Picking out these features is entirely up to personal preference, so keep an eye out if that interests you.
 </p>
 
 <?php include('footer.php'); ?>
